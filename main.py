bec0a49f1b9cfabd1a538f9682ce7f79bfde9a8d
katawut <katawutu.io@gmail.com>
2023-12-30 15:10:54 +0700

    setup graphql

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import strawberry
 from typing import Union
-
 from fastapi import FastAPI
+from strawberry.asgi import GraphQL
+
+@strawberry.type
+class User:
+  name: str
+  age: int
+
+@strawberry.type
+class Query:
+  @strawberry.field
+  def user(self) -> User:
+    return User(name="Proud", age=26)
+  
+schema = strawberry.Schema(query=Query)
+
+graphql_app = GraphQL(schema)
 
 app = FastAPI()
+app.add_route("/graphql", graphql_app)
+app.add_websocket_route("/graphql", graphql_app)
 
 @app.get("/")
 def read_root():
